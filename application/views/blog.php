90e08d882764eb2d56600c590021e1daf4b518df
DedeSnrwn13 <sunarwandede13@gmail.com>
2021-10-22 14:07:27 +0700

    Default Controller

--- application/views/blog.php
+++ application/views/blog.php
@@ -1,5 +1,19 @@
-<h1>
-	Selamat Datang <?php echo $nama; ?> <br> 
-	Golongan Darah <?php echo $goldarah; ?> <br>  
-	Anda tinggal di <?php echo $alamat; ?> 
-</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Artikel</h1>
+
+	<?php foreach ($blogs as $key => $blog): ?>
+		<div class="blog">
+			<h2><?php echo $blog['title']; ?></h2>
+			<p><?php echo $blog['content']; ?></p>
+		</div>
+	<?php endforeach; ?>
+</body>
+</html>
