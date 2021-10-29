ef243171cbaeec1109f5c8e1ac8ee74196b11a11
DedeSnrwn13 <sunarwandede13@gmail.com>
2021-10-29 13:47:44 +0700

    Search Artikel

--- application/views/blog.php
+++ application/views/blog.php
@@ -9,6 +9,13 @@
 <body>
 	<h1>Artikel</h1>
 
+	<a href="<?php echo site_url('blog/add'); ?>">Tambah Artikel</a>
+
+	<form>
+		<input type="text" name="find" id="">
+		<button type="submit">Cari</button>
+	</form>
+
 	<?php foreach ($blogs as $key => $blog): ?>
 		<div class="blog">
 			<h2>
@@ -16,8 +23,10 @@
 					<?php echo $blog['title']; ?>
 				</a>
 			</h2>
+
 			<a href="<?php echo site_url('blog/edit/'.$blog['id']);?>"> Edit</a>
 			<a href="<?php echo site_url('blog/delete/'.$blog['id']);?>"> Delete</a>
+			
 			<p><?php echo $blog['content']; ?></p>
 		</div>
 	<?php endforeach; ?>
