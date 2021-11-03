e633ce25c827687f560822bf09ba3caf834f86d8
DedeSnrwn13 <sunarwandede13@gmail.com>
2021-11-03 10:23:51 +0700

    Templating Bootstrap Bagian 2

--- application/views/detail.php
+++ application/views/detail.php
@@ -1,2 +1,29 @@
-<h1><?php echo $blog['title']; ?></h1>
-<p><?php echo $blog['content']; ?></p>
+<?php $this->load->view('partials/header'); ?>
+
+<!-- Page Header-->
+<header class="masthead" style="background-image: url('<?php echo base_url(); ?>assets/img/post-bg.jpg')">
+	<div class="container position-relative px-4 px-lg-5">
+		<div class="row gx-4 gx-lg-5 justify-content-center">
+			<div class="col-md-10 col-lg-8 col-xl-7">
+				<div class="post-heading">
+					<h1><?php echo $blog['title']; ?></h1>
+					<span class="meta">
+						Posted on <?php echo date('d M Y', strtotime($blog['date'])); ?>
+					</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
+<!-- Post Content-->
+<article class="mb-4">
+	<div class="container px-4 px-lg-5">
+		<div class="row gx-4 gx-lg-5 justify-content-center">
+			<div class="col-md-10 col-lg-8 col-xl-7">
+				<p><?php echo $blog['content']; ?></p>
+			</div>
+		</div>
+	</div>
+</article>
+
+<?php $this->load->view('partials/footer'); ?>
